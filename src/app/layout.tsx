70a577b99b7b9e6c655b26402a42bd8f7ec969f7
Alejandro <alecoco09j@gmail.com>
2026-01-06 11:05:36 -0500

    changed some things, need resume at the end

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Alejandro Jaimes",
-  description: "",
+  description: "Portfolio of Alejandro Jaimes",
 };
 
 export default function RootLayout({
