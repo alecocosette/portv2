a92f259de9a9ec5486a5bd000e161d5486aec72f
Alejandro <alecoco09j@gmail.com>
2026-08-03 18:27:33 -0400

    public images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,11 +56,24 @@ export const metadata: Metadata = {
             "Software Engineer Intern at SREAL in the Institute for Simulation and Training, President of Graphics Programming Knights, Outreach Member of Knight Hacks, UCF Student of Computer Science, and a Venezuelan in Tech",
         siteName: "Alejandro Jaimes",
         images: [
-            {
-                url: "/propcrop.jpg",
-                alt: "Alejandro Jaimes - Software Engineer",
-            },
+            { url: "https://alejaimes.dev/propcrop.jpg", width: 800, height: 600, alt: "Alejandro Jaimes - Software Engineer" },
+            { url: "https://alejaimes.dev/headshot.jpg", width: 800, height: 600, alt: "Alejandro Jaimes Headshot" },
+            { url: "https://alejaimes.dev/alejaimes.jpg", width: 800, height: 600, alt: "Alejandro Jaimes" },
+            { url: "https://alejaimes.dev/alejandroJaimesTK.jpg", width: 800, height: 600, alt: "Alejandro Jaimes alongside TK" },
+            { url: "https://alejaimes.dev/srealShow.webp", width: 800, height: 600, alt: "Alejandro Jaimes at SREAL" },
+            { url: "https://alejaimes.dev/shellhacks1.webp", width: 800, height: 600, alt: "Alejandro Jaimes winning at Shell Hacks 2025" },
+            { url: "https://alejaimes.dev/WINPROJL.jpg", width: 800, height: 600, alt: "Alejandro Jaimes winning at Project Launch 2026" },
+            { url: "https://alejaimes.dev/winBloomKnights.webp", width: 800, height: 600, alt: "Alejandro Jaimes winning at Bloom Knights" },
+            { url: "https://alejaimes.dev/bloomknightsale.webp", width: 800, height: 600, alt: "Alejandro Jaimes at Bloom Knights" },
+            { url: "https://alejaimes.dev/workshopImage.jpg", width: 800, height: 600, alt: "Alejandro Jaimes presenting Math Workshop" },
+            { url: "https://alejaimes.dev/greghouse.webp", width: 800, height: 600, alt: "greghouse project by Alejandro Jaimes" },
         ],
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "Alejandro Jaimes",
+        description: "Software Engineer Intern at SREAL in the Institute for Simulation and Training, President of Graphics Programming Knights, Outreach Member of Knight Hacks, UCF Student of Computer Science, and a Venezuelan in Tech",
+        images: ["https://alejaimes.dev/propcrop.jpg"],
     },
   
 };
